73aa5d4039fbc6f6002062b1902f58be9bfdf11f
eztux <racer851@tamu.edu>
2018-12-06 11:32:18 -0600

    Created a mongoose connection using the config file. (PM me for the config file. It comes with sensitive info.)

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     express = require("express"),
     path = require("path"),
+    monConnect = require("./config.js"),
     app = express();
+
+mongoose.connect("mongodb://" + monConnect.username + ":" + monConnect.pass + "@ds055545.mlab.com:55545/dog-nutrition-plus");
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "views/landing.html"));
@@ -10,4 +13,4 @@ app.get("/", function(req, res){
 
 app.listen(3000, function(req, res){
     console.log("The server is running on port 3000!");
-})
+});
